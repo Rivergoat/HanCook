c95d125a489b1db4f910d1ff349d1de0056f0276
Rivergoat <pelle.haenel@outlook.de>
2017-10-26 20:36:39 +0200

    Added todos

--- HanCook/Hide.cs
+++ HanCook/Hide.cs
@@ -15,8 +15,16 @@ namespace HanCook
          *      Two hashes are calculated, one for X values and one for Y
          *      *       *       *       *       *       *       *       *       *
          *      Ploblem 1: Coordinate Collision, -unlikely, but present
-         *      Problem 2: Linear Matrix search is computational Expensive
+         *      Problem 2: Linear Matrix (To avoid collisions) search is computational Expensive
          * 
+         *      Seperate the image into part with a size of 100x100 and calculate hi-
+         *      ding hashes for each segment
+         *          
+         *      look up how many letters go in each quadrant. Seperate this
+         *      
+         *      ALSO TODO: 
+         *
+         *
          *************************************************************************/
     }
 }
