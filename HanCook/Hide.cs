8efb80ab2dec73244fc5c990b1d0f68bcc1403ad
Rivergoat <pelle.haenel@outlook.de>
2017-10-26 21:01:31 +0200

    Shitted the fuck

--- HanCook/Hide.cs
+++ HanCook/Hide.cs
@@ -10,21 +10,23 @@ namespace HanCook
 {
     class Hide
     {
-        /*************************************************************************
-         *                  How to write ImageMapping file
-         *      Two hashes are calculated, one for X values and one for Y
-         *      *       *       *       *       *       *       *       *       *
-         *      Ploblem 1: Coordinate Collision, -unlikely, but present
-         *      Problem 2: Linear Matrix (To avoid collisions) search is computational Expensive
-         * 
-         *      Seperate the image into part with a size of 100x100 and calculate hi-
-         *      ding hashes for each segment
-         *          
-         *      look up how many letters go in each quadrant. Seperate this
-         *      
-         *      ALSO TODO: 
+        /********************************************************************************
+         *                  How to write ImageMapping file                              *
+         *      Two hashes are calculated, one for X values and one for Y               *
+         *      *       *       *       *       *       *       *       *       *       *
+         *      Ploblem 1: Coordinate Collision, -unlikely, but present                 *
+         *      Problem 2: Linear Matrix (To avoid collisions) search is                *
+         *          computational Expensive                                             *
+         *                                                                              *
+         *      Seperate the image into part with a size of 100x100 and calculate hi-   *
+         *      ding hashes for each segment                                            *
+         *                                                                              *
+         *      look up how many letters go in each quadrant. Seperate this             *
+         *                                                                              *
+         *      ALSO TODO:  Generate artificial Image noise                             *
+         *      FIXME:  Save as png file                                                *      
          *
-         *
-         *************************************************************************/
+         ******************************************************************************/
+
     }
 }
